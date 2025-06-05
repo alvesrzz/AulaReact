98a739110f54863607d1cc0456ea9908f02be60d
Gabriel Alves <gabriel.carvalho1524@gmail.com>
2025-06-05 11:05:30 -0300

    Update Exercicio1.jsx

--- estudocss/src/paginas/Exercicio1.jsx
+++ estudocss/src/paginas/Exercicio1.jsx
@@ -4,12 +4,12 @@ export default function Exercicio1()
 {
     return (
         <div>
-            <h2 className="titulo">Exemplo CSS Puro</h2>
+            <h2 style={{color:"red", backgroundColor:"yellow", textAlign:"center", padding:"10px"}}>Exemplo CSS Puro</h2>
 
             <p>
                 Pode-se aplicar através de <span className="destaque">Folhas 
                 de Estilo</span>, crie-se um arquivo separado com a extenção css, 
-                e depois importa-o no arquivo do react com a extensão jsx.
+                e depois importa-o no arquivo do <span style={{fontStyle:"italic"}}>react</span> com a extensão jsx.
             </p>
         </div>
     )
